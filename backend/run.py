2b1bc12b2ab0637ec1b28bb6fffe67882b049bf5
alefwesleimoreira <alefwesleimoreira@gmail.com>
2026-02-16 10:52:54 -0300

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -5,8 +5,6 @@ import os
 
 app = create_app()
 
-inicializar_banco()
-
 def inicializar_banco():
     with app.app_context():
         db.create_all()
@@ -137,6 +135,8 @@ def make_shell_context():
         'MovimentacaoEstoque': MovimentacaoEstoque,
         'Financeiro': Financeiro
     }
+
+inicializar_banco()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
